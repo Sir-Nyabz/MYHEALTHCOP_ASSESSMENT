ced38d2f7b2bd755673fe2d124a8da8e4fcfa9ef
Isaac Nyaabila <nyaabilai@gmail.com>
2023-08-22 01:24:47 +0000

    changes

--- src/app/first-c/first-c.component.ts
+++ src/app/first-c/first-c.component.ts
@@ -4,8 +4,7 @@ import * as am4maps from '@amcharts/amcharts4/maps';
 import am4geodata_ghanaLow from '@amcharts/amcharts4-geodata/ghanaLow';
 import * as am4charts from "@amcharts/amcharts4/charts";
 import { gahanaMapData } from './dummy-data/ghana-map-data';
-// @ts-ignore
-import ApexCharts from 'apexcharts';
+
 
 
 
@@ -59,7 +58,7 @@ export class FIRSTCComponent implements OnInit,AfterViewInit {
       const pieSeries = chart1.series.push(new am4charts.PieSeries());
       pieSeries.dataFields.value = 'litres';
       pieSeries.dataFields.category = 'country';
-      pieSeries.ticks.template.disabled = true;
+      pieSeries.ticks.template.disabled = false;
   pieSeries.alignLabels = false;
   pieSeries.labels.template.text = "";
   pieSeries.labels.template.radius = am4core.percent(-40);
@@ -78,6 +77,8 @@ export class FIRSTCComponent implements OnInit,AfterViewInit {
     // Set projection
     chart.projection = new am4maps.projections.Miller();
 
+   
+
     // Create map polygon series
     const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 
@@ -87,14 +88,21 @@ export class FIRSTCComponent implements OnInit,AfterViewInit {
     // Make map load polygon (like country names) data from GeoJSON
     polygonSeries.useGeodata = true;
 
+
+
     // Configure series
     const polygonTemplate = polygonSeries.mapPolygons.template;
     polygonTemplate.tooltipText = '{name}: {value}';
+    polygonTemplate.propertyFields.id = "{name}:{id}";
     polygonTemplate.fill = am4core.color('#19105e');
 
     // Create hover state and set alternative fill color
     const hs = polygonTemplate.states.create('hover');
     hs.properties.fill = am4core.color('#554d8c');
+
+     //legend
+     chart.legend = new am4maps.Legend();
+     chart.legend.position = "left";
 
     // Default heat map, uses specified color to populate map
     polygonSeries.heatRules.push({
