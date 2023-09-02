adaae5d0afd41ceae42ee6a9e16fd7433354bf08
Isaac Nyaabila <nyaabilai@gmail.com>
2023-09-02 04:19:42 +0000

    modification in json file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RESPONSIVE-SIDE-BAR'`, () => {
+  it(`should have as title 'MYHEALTHCOP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RESPONSIVE-SIDE-BAR');
+    expect(app.title).toEqual('MYHEALTHCOP');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('RESPONSIVE-SIDE-BAR app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MYHEALTHCOP app is running!');
   });
 });
