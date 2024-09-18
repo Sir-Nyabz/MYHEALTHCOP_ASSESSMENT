16bcfc2f5b4350f214fbc392505f10c633d65c88
Isaac Nyaabila <nyaabilai@gmail.com>
2024-09-18 00:36:09 +0000

    18/09/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,23 +19,39 @@ export class AppComponent{
     setTimeout(()=>{
       this.loader=false
           },3000);
-          
-    let arrow = document.querySelectorAll(".arrow");
-    let sidebar = document.querySelector(".sidebar");
-    let sidebarBtn= document.querySelector(".collapse-icon");
-    for (var i = 0; i < arrow.length; i++) {
-      arrow[i].addEventListener("click", (e:any)=>{
-     let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-     arrowParent.classList.toggle("showMenu");
-      });
-    }
-    
-      console.log(sidebarBtn);
-       sidebarBtn!.addEventListener("click", ()=>{
-       sidebar!.classList.toggle("close");
+
+          const menuToggle = document.getElementById('menu-toggle');
+    const sidebar = document.getElementById('sidebar');
+    const pageContentWrapper = document.getElementById('page-content-wrapper');
+
+    menuToggle?.addEventListener('click', () => {
+      sidebar?.classList.toggle('active');
+      pageContentWrapper?.classList.toggle('toggled');
     });
+  
+          
+    // let arrow = document.querySelectorAll(".arrow");
+    // let sidebar = document.querySelector(".sidebar");
+    // let sidebarBtn= document.querySelector(".bi-list");
+    // for (var i = 0; i < arrow.length; i++) {
+    //   arrow[i].addEventListener("click", (e:any)=>{
+    //  let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+    //  arrowParent.classList.toggle("showMenu");
+    //   });
+    // }
+    
+    //   console.log(sidebarBtn);
+    //    sidebarBtn!.addEventListener("click", ()=>{
+    //    sidebar!.classList.toggle("close");
+    // });
 
     
+  }
+
+  isSidebarVisible: boolean = true;
+
+  toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible;
   }
 
 }
